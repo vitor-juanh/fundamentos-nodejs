f995efbec090842df079424b8f2208f87c393fc6
Vitor Juanh <vitor.juanh@gmail.com>
2023-08-26 22:25:00 -0400

    add withdraw route and get balance function

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.json());
 
 const customers = [];
-// Middleware
 
+// Middleware
 function verifyIfExistsAccountCPF(request, response, next) {
     const { cpf } = request.headers;
 
@@ -22,6 +22,23 @@ function verifyIfExistsAccountCPF(request, response, next) {
     request.customer = customer;
 
     return next();
+}
+
+// Get balance
+/**
+ * statement - []
+ */
+function getBalance(statement) {
+
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === 'credit') {
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
 }
 
 // Routes
@@ -78,6 +95,33 @@ app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
         description,
         amount,
         type: "credit",
+        created_at: new Date(),
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
+// Withdraw
+/**
+ * amount - number
+ * type - string
+ * created_at - date
+ */
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if (balance < amount) {
+        response.status(400).json({ error: "Insufficient funds!" });
+    }
+
+    const statementOperation = {
+        amount,
+        type: "debit",
         created_at: new Date(),
     };
 
